a81175ce782042f306fae47446284aa6326c2a70
Sandun Tharuka <sandun.suntws@gmail.com>
2023-02-22 22:10:48 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+the current count :{{  count}}
+  </div>
+  <button @click="handleClick">+</button>
+  <button @click="count--">-</button>
+</template>
+
+
+<script setup>
+import { ref } from 'vue';
+
+
+let count =ref(0)
+console.log(count);
+const handleClick =()=>{
+ count.value++
+}
+
+</script>
